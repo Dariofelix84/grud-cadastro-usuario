c36a6bd97f0e70b6668f56eb4a4f88ba3b86d781
Dariofelix84 <dariofa69@gmail.com>
2025-06-17 17:50:19 -0300

    criação do método atualizarUsuarioPorEmail

--- src/main/java/com/felix/grud_cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/felix/grud_cadastro_usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+    //Injeção de dependência manual, tem mais duas maneiras
     private final UsuarioRepository repository;
 
     public UsuarioService(UsuarioRepository repository) {
@@ -16,10 +17,21 @@ public class UsuarioService {
         repository.saveAndFlush(usuario);
     }
 
-    //Opção de buscar usuario por Email para teste
+    //Opção de buscar usuário por Email para teste
     public Usuario buscarUsuarioPorEmail(String email) {
         return repository.findByEmail(email).orElseThrow(
-                () ->  new RuntimeException("Email não encontrado")
+                () -> new RuntimeException("Email não encontrado")
         );
+    }
+
+    //Melhor maneira é com MapStruct
+    public void atualizarUsuarioPorEmail(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Usuário não encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
     }
 }
